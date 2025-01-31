177bcf647c3c5e2b525b507f7a191659c4f7513a
Dario-Hesami <Dario.Academic@gmail.com>
2025-01-31 11:40:46 -0500

    Week 4 - P2 - Models - Complete

--- DotNet Bookstore/Models/Category.cs
+++ DotNet Bookstore/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+
 using System.ComponentModel.DataAnnotations;
 
 namespace DotNet_Bookstore.Models
@@ -6,10 +7,13 @@ namespace DotNet_Bookstore.Models
     public class Category
     {
         // pk field should always be called either Id or <ClassName>Id e.g. CategoryId
-        [Display(Name = "Category ID")]
-        [Range(1, 999999)]
+        //[Display(Name = "Category ID")]
+        //[Range(1, 999999)]
         public int CategoryId { get; set; }
         [Required(ErrorMessage = "A customized error message")]
         public string Name { get; set; }
-    }
+
+        // Navigation property - child reference
+        public List<Book> Books { get; set; }
+}
 }

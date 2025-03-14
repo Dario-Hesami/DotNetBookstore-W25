14c7f0e4a6fde2e01414f2b8135238f76acdaac2
Dario Hesami <hossein.hesami@georgiancollege.ca>
2025-03-14 10:36:09 -0400

    Week 9 - P1 - Authorization (Books) - ShopController

--- DotNet Bookstore/Controllers/BooksController.cs
+++ DotNet Bookstore/Controllers/BooksController.cs
@@ -7,9 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DotNet_Bookstore.Data;
 using DotNet_Bookstore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DotNet_Bookstore.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class BooksController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -27,6 +30,7 @@ namespace DotNet_Bookstore.Controllers
         }
 
         // GET: Books/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
---
+++ DotNet Bookstore/Controllers/ShopController.cs
@@ -0,0 +1,22 @@
+﻿using DotNet_Bookstore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_Bookstore.Controllers
+{
+    public class ShopController : Controller
+    {
+        // Class level DBContext connenction object
+        private readonly ApplicationDbContext _context;
+
+        // Constructor that accepts the DBContext object
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}

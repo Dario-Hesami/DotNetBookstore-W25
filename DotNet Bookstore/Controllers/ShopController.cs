328ff24a02b7131bf3fe63176c1a87faf83b4ac5
Dario Hesami <hossein.hesami@georgiancollege.ca>
2025-03-21 10:05:39 -0400

    Week 10 - P1 - Shopping Cart

--- DotNet Bookstore/Controllers/ShopController.cs
+++ DotNet Bookstore/Controllers/ShopController.cs
@@ -18,5 +18,27 @@ namespace DotNet_Bookstore.Controllers
             var categories = _context.Categories.OrderBy(c => c.Name).ToList();
             return View(categories);
         }
+
+        //  Get: /Shop/ShopByCategory/5
+        public IActionResult ShopByCategory(int id)
+        {
+            // Display the category name on the page based on the ID - store category name in the ViewData object
+            var category = _context.Categories.Find(id);
+
+            // return to /shop/index if category not found
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewData["Category"] = category.Name;
+
+            // query the books filtered by the selected CategoryID parameter
+            var books = _context.Books.Where(b => b.CategoryId == id)
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            // send the books to the view for display   
+            return View(books); 
+        }
     }
 }
